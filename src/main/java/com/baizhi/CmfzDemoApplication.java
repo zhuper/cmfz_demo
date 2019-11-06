85a5d19c7c847bc32198a22508e1d655a1560e39
zy <15542725575@163.com>
2019-11-06 11:45:49 +0800

    hehe2

--- src/main/java/com/baizhi/CmfzDemoApplication.java
+++ src/main/java/com/baizhi/CmfzDemoApplication.java
@@ -17,6 +17,7 @@ public class CmfzDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(CmfzDemoApplication.class, args);
         System.out.println("hehehehehehe--");
+        System.out.println("????????");
 
     }
 
